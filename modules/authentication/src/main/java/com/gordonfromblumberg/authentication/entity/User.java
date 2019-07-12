d8bc2c0344d2c965fe5a5cf32b5ee9206c5a1ed7
Aleksandr Ivko <ivko@haulmont.com>
2019-07-12 15:33:42 +0400

    Add persistance dependency and create User entity

---
+++ modules/authentication/src/main/java/com/gordonfromblumberg/authentication/entity/User.java
@@ -0,0 +1,18 @@
+package com.gordonfromblumberg.authentication.entity;
+
+/**
+ * Project: Authentication
+ *
+ * @author: Aleksandr Ivko
+ * Created: 12.07.19
+ * <p>
+ * $Id$
+ */
+
+import java.io.Serializable;
+import javax.persistance.Entity;
+
+@Entity
+public class User implements Serializable {
+
+}
